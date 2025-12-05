28d95ce3fc5718d8a7045b5a368536bd9e2d3d41
Johannes Rothe <mail@johannes-rothe.de>
2025-12-05 22:18:02 +0100

    openwebui oidc config

--- hosts/werkstatt-workstation/llama.nix
+++ hosts/werkstatt-workstation/llama.nix
@@ -22,6 +22,13 @@
       OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
       OLLAMA_BASE_URL = "http://127.0.0.1:11434";
       ENABLE_VERSION_UPDATE_CHECK = "False";
+      WEBUI_URL = "https://open-webui.erfindergeist.org";
+      WEBUI_SECRET_KEY = "";
+      OAUTH_CLIENT_ID = "";
+      OAUTH_CLIENT_SECRET = "";
+      OPENID_PROVIDER_URL = "";
+      OAUTH_PROVIDER_NAME = "";
+      OPENID_REDIRECT_URI = "https://open-webui.erfindergeist.org/oauth/oidc/callback";
     };
   };
 }
