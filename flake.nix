533db900137dbef1797de0ee27a395b0afcdd4f8
Johannes Rothe <mail@johannes-rothe.de>
2026-06-30 19:47:44 +0200

    Update to 26.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A flake describing erfindergeist systems";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
